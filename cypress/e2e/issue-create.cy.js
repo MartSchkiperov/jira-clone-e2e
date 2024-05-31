0818b527df304d9ac7ea0204dee83480e8475c64
MartSchkiperov <martpikk@gmail.com>
2024-05-31 10:44:45 +0300

    Task 2 faker

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -1,6 +1,4 @@
 import { faker } from '@faker-js/faker';
-const randomTitle = faker.lorem.word();
-const randomDescription = faker.lorem.words({ min: 1, max: 10 });
 
 describe('Issue create', () => {
   beforeEach(() => {
